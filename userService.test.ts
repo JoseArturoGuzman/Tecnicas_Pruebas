66b21497e301ce35f537427a9f3b5b96666ea3a8
Jose Arturo Guzman <125223327+JoseArturoGuzman@users.noreply.github.com>
2025-03-03 19:18:13 -0400

    Add: Las pruebas Basadas en el Uso.

--- userService.test.ts
+++ userService.test.ts
@@ -20,8 +20,35 @@ test('Se debe poder registrar un usuario si los datos son correctos', () => {
 });
 
 });
+
+
 describe('Use Based Tests', () => {
 // Desarrollar aquí pruebas Basadas en el Uso
+test('Se debe regsistrar un usuario de manera exitosa, por lo que autenticamos si fue registrado', () => {
+    register('usuario3', 'contrasena123');
+    expect(authenticate('usuario3', 'contrasena123')).toBe(true);
+});
+
+test('Debe de darme un errror cuando intento a un registrar un usuario ya existente', () => {
+    expect(() => register('usuario3', 'contrasena123')).toThrow('User already exists');
+});
+
+test('Debe no dejar autenticar a un usuario que no se haya registrado', () => {
+    expect(authenticate('usuarioNoNuevo', 'Consentraseñatest')).toBe(false);
+});
+
+test('Se debe poder bloquear a un usuario', () => {
+    register('usuarioPrueba', 'contraseña123');
+    blockUser('usuarioPrueba');
+    expect(authenticate('usuarioPrueba', 'contraseña123')).toBe(false);
+})
+
+test('Se debe de eliminar a los usuarios registrados cuando se resetea, y por lo tanto no se podrán autenticar', () => {
+    register('usuarioTest', 'contraseña123');
+    reset();
+    expect(authenticate('usuarioTest', 'contraseña123')).toBe(false);
+});
+
 });
 describe('Model Based Tests', () => {
 // Desarrollar aquí pruebas Basadas en Modelos
